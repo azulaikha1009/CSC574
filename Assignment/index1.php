f6627fb78000156b8ad1e7c441fd44e2468eadef
azulaikha1009 <arifahzulaikha1009@gmail.com>
2026-07-08 13:12:13 +0800

    Add files via upload

---
+++ Assignment/index1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<meta charset = "UTF=8">
+		<title>Projek Pertama Saya</title>
+		</head>
+		
+		<body>
+		<p>Ini adalah laman web saya yang pertama!</p>
+		</body>
+	</html>
